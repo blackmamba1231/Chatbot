f3e70446d924be734581fc63aae7d06d9857ab15
sudhanshu tiwary <116373262+blackmamba1231@users.noreply.github.com>
2024-09-28 23:37:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'client',
+   root: './client', // Adjust this path if your index.html is elsewhere
+  build: {
+    outDir: 'dist', // Specify the output directory if needed
+  },
   plugins: [react()],
 })
