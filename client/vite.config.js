009953f4dff8bd757f34d4bb567b5ce2b0cad44b
sudhanshu tiwary <116373262+blackmamba1231@users.noreply.github.com>
2024-09-28 23:41:56 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   root: './client', // Adjust this path if your index.html is elsewhere
-  build: {
-    outDir: 'dist', // Specify the output directory if needed
-  },
   plugins: [react()],
-})
+  root: './client', // Adjust this if needed
+  build: {
+    outDir: 'dist', // Output directory for the build
+  },
+});
