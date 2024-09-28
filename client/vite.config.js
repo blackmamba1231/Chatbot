9fc2df33be5cbaf2b88639f70afa548875074e04
sudhanshu tiwary <sudhanshukumar321456@gmail.com>
2024-09-29 00:20:57 +0530

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: '../dist', // or wherever you want to output your build files
+    rollupOptions: {
+      input: './client/index.html', // Update this path as needed
+    },
+  },
+});
