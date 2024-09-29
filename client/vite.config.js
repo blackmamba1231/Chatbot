9befc05a6c5b2dc118c35d29dd7e55c3faeb3ee8
sudhanshu tiwary <116373262+blackmamba1231@users.noreply.github.com>
2024-09-29 11:31:43 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,15 +8,7 @@ export default defineConfig({
   build: {
     outDir: '../dist', // or wherever you want to output your build files
     rollupOptions: {
-      input: './client/index.html', // Update this path as needed
+      input: '../client/index.html', // Update this path as needed
     },
 =======
 
-export default defineConfig({
-  plugins: [react()],
-  
-  build: {
-    outDir: 'dist', // Output directory for the build
->>>>>>> e9c646ab6c8aafa62e96873eb7b6909e5f0e67d0
-  },
-});
