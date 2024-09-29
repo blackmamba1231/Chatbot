c6965d26cfad68fe8533a8688570fc6a0136de11
sudhanshu tiwary <116373262+blackmamba1231@users.noreply.github.com>
2024-09-29 11:40:37 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-<<<<<<< HEAD
-
 
 export default defineConfig({
   plugins: [react()],
-  root: './index.html',
-=======
-
+});
